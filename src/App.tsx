69a9fd6a33e1fdf97c2f63d810a2880661982554
Luis Felipe Rodrigues Bruno <lf210400@gmail.com>
2025-11-21 17:18:28 -0300

    retirando tema

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { TooltipProvider } from "./components/ui/tooltip.tsx";
 import { Toaster } from "./components/ui/toaster.tsx";
-import { ThemeProvider } from "./components/ThemeProvider.tsx";
 import { MessagesProvider } from "./context/MessagesContext.tsx";
 import { HelmetProvider } from 'react-helmet-async';
 
@@ -118,7 +117,7 @@ const AppContent = () => {
 const App = () => {
     return (
         <QueryClientProvider client={queryClient}>
-            <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme" attribute="class">
+
                 <TooltipProvider>
                     <Toaster />
                     <HelmetProvider>
@@ -127,7 +126,7 @@ const App = () => {
                         </BrowserRouter>
                     </HelmetProvider>
                 </TooltipProvider>
-            </ThemeProvider>
+
         </QueryClientProvider>
     );
 };
